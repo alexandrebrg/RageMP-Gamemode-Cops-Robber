b9db082b831b74936570da8ec50f570a305547e1
Protocole63 <alexandre.asto63@orange.fr>
2018-08-02 12:36:41 +0200

    Client-Side Add showBottomText method

--- client_packages/gamemode/modules/Notify.js
+++ client_packages/gamemode/modules/Notify.js
@@ -1,6 +1,9 @@
 const _SET_NOTIFICATION_COLOR_NEXT = "0xf92b835a141c6bdd";
 const _SET_NOTIFICATION_BACKGROUND_COLOR = "0xa633a5db2d269825";
 const maxStringLength = 99;
+var bottomText = null;
+var bottomTextTime = -1;
+var bottomTextInterval = null;
 
 mp.events.add("BN_Show", (message, flashing = false, textColor = -1, bgColor = -1, flashColor = [77, 77, 77, 200]) => {
     if (textColor > -1) mp.game.invoke(_SET_NOTIFICATION_COLOR_NEXT, textColor);
@@ -27,3 +30,21 @@ mp.game.ui.notifications = {
     show: (message, flashing = false, textColor = -1, bgColor = -1, flashColor = [77, 77, 77, 200]) => mp.events.call("BN_Show", message, flashing, textColor, bgColor, flashColor),
     showWithPicture: (title, sender, message, notifPic, icon = 0, flashing = false, textColor = -1, bgColor = -1, flashColor = [77, 77, 77, 200]) => mp.events.call("BN_ShowWithPicture", title, sender, message, notifPic, icon, flashing, textColor, bgColor, flashColor)
 };
+
+mp.events.add("showBottomText", (message, time = 5000) => {
+    bottomText = message;
+    bottomTextTime = time;
+    bottomTextInterval = setInterval(() => {
+        if(bottomTextTime == 0) { clearInterval(bottomTextInterval); bottomTextInterval = null; return bottomTextTime = -1 }
+        bottomTextTime--;
+    }, 1000);
+});
+
+mp.events.add("render", () => {
+    if(bottomTextTime != -1) {
+        mp.game.ui.setTextFont(7);
+        mp.game.ui.setTextEntry2("STRING");
+        mp.game.ui.addTextComponentSubstringPlayerName(bottomText);
+        mp.game.ui.drawSubtitleTimed(1, true);
+    }
+});

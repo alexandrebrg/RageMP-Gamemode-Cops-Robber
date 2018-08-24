33e1907c1a740bff2d69543318396898bca512d9
Protocole63 <alexandre.asto63@orange.fr>
2018-08-24 15:47:15 +0200

    Server-Side Delete some comments

--- packages/gamemode/modules/db.js
+++ packages/gamemode/modules/db.js
@@ -4,7 +4,7 @@ module.exports = {
 	Handle: null,
     Connect: function(callback) {
         this.Handle = mysql.createConnection({
-            host     : 'localhost', //do not use ip address as a host, create a dns if your mysql server is not hosted on same server/machine as ragemp server
+            host     : 'localhost', 
             user     : 'root',
             password : '',
             database : 'gta5'

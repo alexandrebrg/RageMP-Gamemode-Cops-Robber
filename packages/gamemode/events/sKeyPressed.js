36f61795702a712cb899d1f61fd4b341eb1397ae
Protocole63 <alexandre.asto63@orange.fr>
2018-08-24 15:33:22 +0200

    Server-Side Fix some poo with commits

--- packages/gamemode/events/sKeyPressed.js
+++ packages/gamemode/events/sKeyPressed.js
@@ -1,62 +1,68 @@
-const Players = require('../modules/players');
-const PM = require('../messages/player.json');
-const Factions = require('../modules/factions');
-const Ammunation = require('../data/ammunation.json');
+var Players = require('../modules/players');
+var PM = require('../messages/player.json')
+var Factions = require('../modules/factions')
+var Ammunation = require('../data/ammunation.json')
 
 module.exports = {
-      let done = false;
-      mp.markers.forEachInRange(player.position, 3, (b) => {
-        if(b.store && !b.robbed) {
-          switch(b.storeType) {
-            case 1:
-              player.call("247choice", [b.sqlid]);
-              break;
-            case 2:
-              player.call("AMMUNATIONchoice", [b.sqlid, JSON.stringify(Ammunation[b.sqlid])]);
-              break;
-          }
-          return false;
-        }
-        else if(b.store && b.robbed) {
-          player.notify("~r~(!) ~w~Store already robbed, wait some time!");
-        }
-        else if(b.teleporter && b.dimension === player.dimension && !done) {
-          if(b.faction !== 1 && Players.PlayerHaveAccess(player.ID, b.faction) && !player.adminID) return player.notify(PM.AccessTP)
-          player.call("fadeOut");
-          setTimeout(function() {
-            let veh;
-            let seat;
-            if(b.veh && player.vehicle) {
-              veh = player.vehicle;
-              seat = player.seat;
-              player.vehicle.position = b.to;
-            } else if(!b.veh && player.vehicle) return player.notify(PM.IsInVehicle);
-            player.dimension = b.to_dimension;
-            player.position = b.to;
-            if(b.veh && typeof veh !== "undefined") {
-              player.putIntoVehicle(veh, seat);
-              player.vehicle.heading = b.to_angle;
-            }
-            player.heading = b.to_angle;
-            player.call("fadeIn");
-          }, 1000);
+    "sKeyPressed" : (player, key) => {
+        if(key=="Y") {
 
-          done = true;
-          return false;
-        } else if(b.arrest) {
-          mp.players.forEachInRange(player.position, 5, (p2) => {
-            if(Players.isPlayerCuffed(p2.ID) && Factions.isFactionCops(Players.getPlayerFaction(player.ID))) {
-              mp.events.call("PutPlayerInJail", player, p2);
-            }
-          });
-        } else if(b.atm) {
-          player.call("ATMChoice", [b.id]);
-        } else if(b.custom) {
-          mp.events.call("showVehicleCustom", player, b.customID);
-        } else if(b.carShop){
-          mp.events.call("showVehicleShop", player, b.carShopType);
+            var done = false;
+            mp.markers.forEachInRange(player.position, 3, (b) => {
+                if(b.store && !b.robbed) {
+                    switch(b.storeType) {
+                        case 1:
+                            player.call("247choice", [b.sqlid]);
+                            break;
+                        case 2:
+                            player.call("AMMUNATIONchoice", [b.sqlid, JSON.stringify(Ammunation[b.sqlid])]);
+                            break;
+                    }
+                    return false;
+                }
+                else if(b.store && b.robbed) {
+                    player.notify("~r~(!) ~w~Store already robbed, wait some time!");
+                }
+                else if(b.teleporter && b.dimension == player.dimension && !done) {
+                    if(b.faction != 1 && Players.PlayerHaveAccess(player.ID, b.faction) && !player.adminID) return player.notify(PM.AccessTP)
+                    player.call("fadeOut");
+                    setTimeout(function() {
+                        let veh;
+                        let seat;
+                        if(b.veh && player.vehicle) {
+                            veh = player.vehicle;
+                            seat = player.seat;
+                            player.vehicle.position = b.to;
+                        } else if(!b.veh && player.vehicle) return player.notify(PM.IsInVehicle)
+                        player.dimension = b.to_dimension;
+                        player.position = b.to;
+                        if(b.veh && typeof veh !== "undefined") {
+                            player.putIntoVehicle(veh, seat)
+                            player.vehicle.heading = b.to_angle;
+                        }
+                        player.heading = b.to_angle;
+                        player.call("fadeIn");
+                    }, 1000)
+
+
+                    done = true;
+                    return false;
+                } else if(b.arrest) {
+                    mp.players.forEachInRange(player.position, 5, (p2) => {
+                        if(Players.isPlayerCuffed(p2.ID) && Factions.isFactionCops(Players.getPlayerFaction(player.ID))) {
+                            mp.events.call("PutPlayerInJail", player, p2);
+                        }
+                    });
+                } else if(b.atm) {
+                    player.call("ATMChoice", [b.id]);
+                } else if(b.custom) {
+                    mp.events.call("showVehicleCustom", player, b.customID);
+                } else if(b.carShop){                         
+                    mp.events.call("showVehicleShop", player, b.carShopType);
+                }
+                
+            });
+            
         }
-      });
     }
-  }
-};
+}

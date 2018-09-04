9d7d28aa7bb4d171247eac38216d772fe5155c8d
Protocole63 <alexandre.asto63@orange.fr>
2018-09-04 14:26:56 +0200

    New Marker/Blip/ColShape library

---
+++ packages/gamemode/modules/gamepoi.js
@@ -0,0 +1,76 @@
+const Config = require('../data/config.json')
+
+
+class GamePOI {
+
+    constructor(position, scale = 1, options = {}) {
+        this.position = position;
+        this.scale = scale;
+        this.options = options;
+    }
+    /**
+     * Initiate marker
+     */
+    createMarker(markerType = 1, argumentse = {}) {
+        this.markerType = 1;
+        this.markerArguments = argumentse;
+        this.marker = mp.markers.new( markerType, {x: this.position.x, y: this.position.y, z: this.position.z -1}, this.scale, this.markerArguments);
+
+        for(var obj in this.options) {
+            this.marker[obj] = this.options[obj];
+        }
+    }
+    
+    /**
+     * Create colshape
+     */
+    createColshape(shape, autoActivate = false, ...args) {
+        this.colshapeShape = shape;
+        switch(shape) {
+            case "sphere":
+                this.colshape = mp.colshapes.newSphere(this.position.x, this.position.y, this.position.z-1, this.scale);
+                break;
+            case "tube":
+                this.colshapeHeight = args[0];
+                this.colshape = mp.colshapes.newTube(this.position.x, this.position.y, this.position.z-1, this.scale, this.colshapeHeight);
+                break;
+
+            case "rectangle":
+                this.colshapeWidth = args[0];
+                this.colshapeHeight = args[1];
+                this.colshape = mp.colshapes.newRectangle(this.position.x, this.position.y, this.colshapeWidth, this.colshapeHeight);
+                break;
+            
+            case "circle":
+            default:
+                this.colshape = mp.colshapes.newCircle(this.position.x, this.position.y, this.scale-0.5);
+                shape !== "circle" ? console.log("Colshape not valid!") : true;
+                break;
+        }
+        for(var obj in this.options) {
+            this.colshape[obj] = this.options[obj];
+        }
+        this.colshape.autoActivate = autoActivate;
+    }
+
+    /**
+     * Create Label for it
+     */
+    createBlip(blipID, blipColor, blipText, dimension = Config.defaultDimension) {
+        this.blipID = blipID;
+        this.blipColor = blipColor;
+        this.blipText = blipText;
+        this.blip = mp.blips.new(this.blipID, this.position, {
+            dimension: dimension,
+            color: this.blipColor,
+            name: this.blipText
+        });
+
+    }
+}
+    
+module.exports = GamePOI;
+
+mp.events.add("playerEnterColshape", (player, shape) => {
+    mp.events.call("sKeyPressed", player, "Y");
+})

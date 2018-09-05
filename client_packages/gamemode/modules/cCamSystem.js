ed66d68afb1c25104a6b5e02920191f6c9069c8d
Protocole63 <=>
2018-09-05 10:20:40 +0200

    Edit Scaleform

--- client_packages/gamemode/modules/cCamSystem.js
+++ client_packages/gamemode/modules/cCamSystem.js
@@ -2,7 +2,7 @@ const scaleForm = require('/gamemode/modules/lib/scaleForm');
 const camerasManager = require('/gamemode/modules/lib/camerasManager')
 let IsInCamSystem = false;
 
-let CamSystemScaleForm = new scaleForm("security_cam");
+let CamSystemScaleForm = new scaleForm("traffic_cam");
 let CamSystemIntruct = new scaleForm("instructional_buttons");
 let CamSystemActual  = 0;
 let camData = {};
@@ -24,8 +24,9 @@ mp.events.add({
     "initCamSystem": (camDatas) => {
 
         // Security Cam
-        CamSystemScaleForm.callFunction("SET_LOCATION", camDatas.areaname)
-        CamSystemScaleForm.callFunction("SET_TIME", 13,13);
+        CamSystemScaleForm.callFunction("SET_LAT_LONG", 27, 07, 14, 23)
+        CamSystemScaleForm.callFunction("SET_CAM_DATE", 2, 16, 15);
+        CamSystemScaleForm.callFunction("PLAY_CAM_MOVIE");
         camData = camDatas.cameras;
 
         // Instructionals

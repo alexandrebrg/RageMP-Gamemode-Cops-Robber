0d5424c3145cb4f85137ad2addbd4f4bd3354457
Protocole63 <=>
2018-09-08 19:16:02 +0200

    Reorganise Robbery

--- client_packages/gamemode/modules/cPlayer.js
+++ client_packages/gamemode/modules/cPlayer.js
@@ -25,46 +25,6 @@ mp.events.add({
     "fadeIn": () => {
         mp.game.cam.doScreenFadeIn(1000);
     },
-    "InterruptRobberyTimer": () => {
-        if(interval) {
-            clearInterval(interval);
-            interval = null;
-        }
-        robberyBar.visible = false;
-        robberyBar = null;
-
-        rewardBar.visible = false;
-        rewardBar = null;
-    },
-    "setRobberyTimer": (time) => {
-        timeLeft = time;
-        mp.events.call("ShowShardMessage", "Robbery started!", "Stay in the checkpoint to not interrupt it!", 0, 6);
-        if(robberyBar == null) {
-            robberyBar = new timerBarLib.TimerBar("TIME LEFT");  
-            robberyBar.text = `${time}`;
-        }
-        interval = setInterval(function() {
-            if(timeLeft == 0) {
-                interval = clearInterval(interval);
-                interval = null;
-            }  else {
-                timeLeft -= 1;  
-                if(timeLeft % 2 === 0) {
-                    robberyBar.textColor = [183, 0, 0, 255];
-                } else {
-                    robberyBar.textColor = [255, 255, 255, 255];
-                }  
-            }
-            robberyBar.text = (timeLeft != 0) ? `${timeLeft}` : `Done`;  
-        }, 1000);
-    },
-    "clearRobberyTimers": () => {
-        robberyBar.visible = false;
-        robberyBar = null;
-        rewardBar.visible = false;
-        return rewardBar = null;
-
-    },
     "setRewardBar": (reward) => {
         if(rewardBar == null) { 
             rewardBar = new timerBarLib.TimerBar("BENEFITS");
--- packages/gamemode/events/sPlayer.js
+++ packages/gamemode/events/sPlayer.js
@@ -7,53 +7,6 @@ const PM = require('../messages/player.json')
 
 module.exports = {
 
-    "PlayerRobberyStart": (player, biz) => {
-        if(Factions.isFactionCops(Players.getPlayerFaction(player.ID))) return player.notify(PM.PlayerIsCopRob);
-        player.robbery.biz = biz;
-        player.robbery.benefits = 100;
-        bizType = Bizs.getBizType(biz);
-        Bizs.setRobbed(biz);
-        timeLeft = 0;
-        player.call("setRewardBar", [player.robbery.benefits]);
-        Players.notifyCops(PM.RobberyInProgress, player.position);
-        if(bizType === 1) {
-            timeLeft = Config.storeType1Time;
-            player.call("setRobberyTimer", [timeLeft]);
-            player.robbery.timeLeft = timeLeft;
-            player.call("setWantedLevel", [Players.updatePlayerWantedLevel(player.ID, 1)]);
-        } else if(bizType === 2) {
-            timeLeft = Config.storeType2Time;
-            player.call("setRobberyTimer", [timeLeft]);
-            player.robbery.timeLeft = timeLeft;
-            player.call("setWantedLevel", [Players.updatePlayerWantedLevel(player.ID, 2)]);
-        }
-
-        player.robbery.interval = setInterval(function() {
-            timeLeft -= 1;
-            mp.markers.forEachInRange(player.position, 10, (b) => {
-                if(b.store && b.sqlid === player.robbery.biz) {
-                    player.robbery.benefits = player.robbery.benefits + Math.floor(Math.random() * Config.storeCashPerSecond);
-                    player.call("setRewardBar", [player.robbery.benefits]);
-                } else {
-                    clearInterval(player.robbery.interval);
-                    mp.events.call("PlayerRobberyInterrupted", player);
-                }
-            });
-
-            if(timeLeft === 0) { mp.events.call("PlayerRobberyDone", player); clearInterval(player.robbery.interval); }
-        }, 1000); 
-        
-    },
-    "PlayerRobberyInterrupted": (player) => {
-        mp.events.call("sCashUpdate", player, player.robbery.benefits);
-        player.call("InterruptRobberyTimer");
-        player.notify(PM.RobberyInterrupted);
-    },
-    "PlayerRobberyDone": (player) => {
-        mp.events.call("sCashUpdate", player, player.robbery.benefits);
-        setTimeout(function() { player.call("clearRobberyTimers"); }, 5000);
-        player.notify(PM.RobberyInterrupted);
-    },
 
 
     "playerQuit" : (player, exitType, reason) => {
